76544aa15fbed7ce0cd3fa301067664994b01948
Jørgen Vartdal Halse <jorgevh@stud.ntnu.no>
2021-09-23 22:15:15 +0200

    map container

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MapContainer from "./Components/MapContainer";
 import { Card } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>This is header</h1>
-        <Card>Test</Card>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <h1>This is header</h1>
+        </header>
+      </div>
+      <MapContainer></MapContainer>
     </div>
+
+    
   );
 }
 
