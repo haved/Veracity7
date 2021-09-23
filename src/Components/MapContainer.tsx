d7d144ca6502426d80df6dfc0e16fe681bfd5e25
hermahs <hermahs@stud.ntnu.no>
2021-09-24 00:29:19 +0200

    lagt til kart

--- src/Components/MapContainer.tsx
+++ src/Components/MapContainer.tsx
@@ -1,13 +1,53 @@
 import * as React from "react";
+import {Feature, Geometry, FeatureCollection } from 'geojson';
+import { feature } from 'topojson-client';
+import { geoEqualEarth, geoPath } from "d3-geo";
 import '../Style/App.css';
 
-const MapContainer  = ()  => {
-    return (
-        <div className="Container" id={"map"}>
-            <h3>Imagine a map here</h3>
+const uuid = require('react-uuid');
 
+const scale: number = 200;
+const cx: number = 400;
+const cy: number = 150;
+
+const MapContainer  = ()  => {
+
+    const [geographies, setGeographies] = React.useState<[] | Array<Feature<Geometry | null>>>([]);
+
+    React.useEffect(() => {
+        fetch('/data/world.json').then((response) => {
+            if(response.status !== 200) {
+                console.log("error" + response.status);
+                return;
+            }
+
+            response.json().then((worldData) => {
+                const mapFeature: Array<Feature<Geometry | null>> = ((feature(worldData, worldData.objects.countries) as unknown) as FeatureCollection).features;
+                setGeographies(mapFeature);
+            })
+
+        })
+    }, []);
+
+    const projection = geoEqualEarth().scale(scale).translate([cx, cy]).rotate([0,0]);
+
+    return (
+        <div>
+            <svg width={scale*3} height={scale*3} viewBox="0 0 800 450">
+                <g>
+                    {(geographies as []).map((d, i) => (
+                        <path key={`path-${uuid()}`} 
+                        d={geoPath().projection(projection)(d) as string}
+                        fill={`rgba(38,50,56,${(1/ (geographies ? geographies.length : 0)) * i})`}
+                        stroke="aliceblue"
+                        strokeWidth={0.5}
+                        />
+                    ))}
+
+                </g>
+            </svg>
         </div>
-    )
+    );
 }
 
 export default MapContainer
