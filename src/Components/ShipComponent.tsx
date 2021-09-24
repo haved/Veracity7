5e3e80f47252530ce55fbb4d57318cb88fc81e53
Jørgen Vartdal Halse <jorgevh@stud.ntnu.no>
2021-09-24 08:28:07 +0200

    kkkk

--- src/Components/ShipComponent.tsx
+++ src/Components/ShipComponent.tsx
@@ -1,26 +1,28 @@
 import * as React from "react";
 import "../Style/App.css";
 import { Card } from "react-bootstrap";
+import { Boat, loadBoats } from "../Boat";
+import "../Boat";
+import "../App";
 
-const ShipComponent  = ()  => {
-    return (
-            <div className="ship-container">
-                <div>
-                    <p>Image</p>
-                </div>
-                <div className="ship-info">
-                    <div>
-                        <h5>Name of ship</h5>
-                    </div>
-                    <div>
-                        <p>Imagine a lot of different information about this ship here.</p>
-                        <p>Speed: I am</p>
-                        <p>Lorem: dimsum</p>
-                    </div>
-                </div>
+const ShipComponent = async () => {
+  return (
+    <div className="ship-container">
+      <div>
+        <p>Image</p>
+      </div>
+      <div className="ship-info">
+        <div>
+          <h5>Name of ship</h5>
+        </div>
+        <div>
+          <p>Imagine a lot of different information about this ship here.</p>
+          <p>Speed: I am</p>
+          <p>Lorem: dimsum</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-            </div>
-    );
-} 
-
-export default ShipComponent
+export default ShipComponent;
