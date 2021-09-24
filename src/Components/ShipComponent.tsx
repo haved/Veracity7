0f3d7ac49d612260f5710639895055302803d0e8
hermahs <hermahs@stud.ntnu.no>
2021-09-24 11:14:37 +0200

    add boat colors

--- src/Components/ShipComponent.tsx
+++ src/Components/ShipComponent.tsx
@@ -7,25 +7,29 @@ import "../App";
 
 const ShipComponent = (props: { boat: Boat }) => {
   const { boat } = props;
+  const [open, setOpen] = React.useState<Boolean>(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  }
 
   return (
-    <div className="ship-container">
-      <div>
-        <p>Image</p>
+    <div className="ship-container" onClick={handleClick}>
+      <div className="image" style={{backgroundColor: boat.color}}>
       </div>
       <div className="ship-info">
         <div className="vessel-name">
           <h5>{boat.vesselName}</h5>
         </div>
-        <div className="information">
+        <div className={`information ${(open) ? "active" : "disable"}`}>
           <p className="info-text">Total Price: </p>
           {Math.round(boat.price)}
         </div>
-        <div className="information">
+        <div className={`information ${(open) ? "active" : "disable"}`}>
           <p className="info-text">Total CO2: </p>
           {Math.round(boat.ladenTrip.totalCO2 + boat.ballastTrip.totalCO2)}
         </div>
-        <div className="information">
+        <div className={`information ${(open) ? "active" : "disable"}`}>
           <p className="info-text">Distance From Me: </p>
           {Math.round(boat.ballastTrip.totalDistance)}
         </div>
